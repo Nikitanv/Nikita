149787ccbe00163ab03aac53c55638992fe0f17a
Nikitanv <54415506+Nikitanv@users.noreply.github.com>
2019-08-22 21:34:40 +0300

    Add files via upload

---
+++ DZ.js
@@ -0,0 +1,51 @@
+
+
+alert("Доброе утро");
+[1,2,3].push();
+console.log('дом');
+
+
+var money = 5;
+console.log('money:', money);
+
+let income =4;
+
+
+const addExpenses = 3;
+
+
+var deposit = 2;
+
+
+let mission = 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
