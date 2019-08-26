1052333ee667a3fcd2f332c5a543598e18024864
Nikitanv <54415506+Nikitanv@users.noreply.github.com>
2019-08-26 14:46:11 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
-let money = 500000;
-let income = ' Фриланс '; //далеко я не ушел 
-let addExpenses = 'Food, Energy, Hospital' ;
-let deposit = true;
-let mission = 200000000000000;
-let period = 5;
-
-let budgetDay = (money / 30);
-let bgd = (money % 30)
-
-console.log ('money =', money);
+'use strict';
 
 
-console.log ('income =', income);
+let Question = prompt('ваш месячный доход?');
+let money = Question;
 
-console.log ('deposit =', deposit);
+let Question2 = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+let addExpenses = Question2;
 
-console.log(income.length);
+let Question3 = confirm('Есть ли у вас депозит в банке?');
+let deposit = Question3;
+
+let income = ' Фриланс ';
+
+let mission = 2000000;
+
+let Question4 = prompt('Какие обязательные ежемесячные расходы у вас есть?')
+let tallage  = Question4;
+
+let Question5 = prompt('Во сколько это обойдется?');
+let allExpenses = Question5;
+
+let budgetMonth = (money - tallage);
+
+let budgetDay =  (budgetMonth / 30);
 
 
-console.log('Период', period, 'лет');
 
-console.log('Цель заработать', mission,'фунтов' );
+console.log('money = ', money);
 
-console.log(addExpenses.toLowerCase())
+console.log('addExpenses = ',addExpenses);
 
+console.log('income = ',income);
 
-console.log(addExpenses.split(', '))
+console.log('deposit = ',deposit);
 
-console.log('dudgetDay', budgetDay);
+console.log('budgetMonth = ', budgetMonth);
 
-console.log('остаток', bgd);
+console.log(Math.round(mission/budgetMonth));
+
+console.log(Math.round(budgetDay));
+
+if (budgetDay >800 ) console.log('Высокий уровень дохода');
+else if (budgetDay >=300 <= 800) console.log('Средний уровень дохода');
+else if (budgetDay >=0 < 300 ) console.log('Низкий уровень дохода');
+else if (budgetDay<0 ) console.log('Что то пошло не так');
