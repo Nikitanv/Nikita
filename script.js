b1627ed8d788114b22483576365d4cd01dfc6ad6
Nikitanv <54415506+Nikitanv@users.noreply.github.com>
2019-08-28 11:16:49 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,32 @@
  'use strict';
 
 
-let money = prompt('ваш месячный доход?');
-
+let money = +prompt('ваш месячный доход?');
 let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-
-
 let deposit = confirm('Есть ли у вас депозит в банке?');
-
 let income = ' Фриланс ';
-
-let mission = 2000000;
-
+let mission = 20000;
 let tallage  = prompt('Какие обязательные ежемесячные расходы у вас есть?');
-
-let allExpenses = prompt('Во сколько это обойдется?');
-
-let budgetMonth = (money - tallage);
-
+let allExpenses = +prompt('Во сколько это обойдется?');
+let budgetMonth = (money - allExpenses);
 let budgetDay =  (budgetMonth / 30);
-
-
+let tallage1  = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+let allExpenses1 = +prompt('Во сколько это обойдется?');
 
 console.log('money = ', money);
-
 console.log('addExprenses = ', String(addExpenses));
-
 console.log('income = ',income);
-
 console.log('deposit = ',deposit);
-
 console.log('budgetMonth = ', budgetMonth);
-
 console.log(Math.ceil(mission/budgetMonth));
-
 console.log(Math.floor(budgetDay));
 
+
 if (budgetDay >800 ) console.log('Высокий уровень дохода');
-else if (budgetDay >=300 <= 800) console.log('Средний уровень дохода');
-else if (budgetDay >=0 < 300 ) console.log('Низкий уровень дохода');
+else if (budgetDay >=300 )console.log('Средний уровень дохода');
+else if (budgetDay >=0  ) console.log('Низкий уровень дохода');
 else if (budgetDay<0 ) console.log('Что-то пошло не так');
+
+
+
+
