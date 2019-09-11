a890490b4cf00cb91fa301e001c82fa039c108e7
Nikitanv <54415506+Nikitanv@users.noreply.github.com>
2019-09-11 19:27:57 +0300

    Add files via upload

---
+++ lesson 7/script/script.js
@@ -0,0 +1,10 @@
+let but = document.querySelector('#start'),
+but2 = document.querySelector('#cancel'),
+plus = document.querySelectorAll('button'),
+line = document.querySelectorAll('input'),
+block = document.querySelectorAll('result');
+console.log(but);
+console.log(but2);
+console.log(plus);
+console.log(line);
+console.log(block);
