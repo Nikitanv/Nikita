9669aa2b34fc4ed9a77c77d0a1d1f4d1e3cfefe4
Nikitanv <54415506+Nikitanv@users.noreply.github.com>
2019-09-12 19:15:35 +0300

    Add files via upload

--- lesson 7/script/script.js
+++ lesson 7/script/script.js
@@ -1,10 +1,23 @@
-let but = document.querySelector('#start'),
-but2 = document.querySelector('#cancel'),
-plus = document.querySelectorAll('button'),
-line = document.querySelectorAll('input'),
-block = document.querySelectorAll('result');
-console.log(but);
-console.log(but2);
-console.log(plus);
-console.log(line);
-console.log(block);
+let start = document.getElementById('start'),
+button = document.getElementsByTagName('button'),
+incomepl = button[0] ,
+expensespl = button[1],
+additionalIncomeItem = document.querySelectorAll('.additional_income-item'),
+additionalExpensesItem = document.querySelector('.additional_expenses-item'),
+targetValue = document.querySelector('.target-amount'),
+depositCheck = document.querySelector('#deposit-check'),
+budgetDayValue = document.getElementsByClassName('budget_day-value')[0],
+budgetMounthValue = document.getElementsByClassName('budget_mounth-value')[0],
+expensesMounthValue = document.getElementsByClassName('expenses_mounth-value')[0],
+additionalIncomeValue = document.getElementsByClassName('additional_income-value')[0],
+additionalMounthValue = document.getElementsByClassName('additional_mounth-value')[0],
+incomePeriodValue = document.getElementsByClassName('income_period-value')[0],
+targetMounthValue = document.getElementsByClassName('target_mounth-value'),
+salaryAmount = document.querySelector('.salary-amount'),
+incomeTitle = document.querySelector('.income-title'),
+incomeAmount = document.querySelector('.income-amount'),
+expensesTitle = document.querySelector('.expenses-title'),
+additionalExpenses = document.querySelector('.additional_expenses'),
+periodSelector = document.querySelector('.period-select'),
+expensesItem = document.querySelectorAll('.expenses-item'),
+incomeItem = document.querySelectorAll('.income-item');
--- you-dont-know-js/script2.js
+++ you-dont-know-js/script2.js
@@ -3,12 +3,12 @@
 
 let elem = document.querySelectorAll('.book'),
 col = document.querySelectorAll('.books'),
-adver = document.querySelectorAll('adv'),
+adver = document.querySelector('.adv'),
 pick = document.querySelector('body'),
-dav = document.querySelectorAll('div'),
 h = document.querySelectorAll('h2'),
-l = document.querySelectorAll('li'),
-newElem = document.createElement('li');
+newElem = document.createElement('li'),
+boo = elem[0].getElementsByTagName('li');
+
 
 console.log(adver);
 console.log(pick);
@@ -22,13 +22,17 @@ pick.setAttribute('style', 'background-image:url(./image/you-dont-know-js.jpg)')
 
 
 console.log(elem);
-console.log(dav);
-console.log(h);
-console.log(l);
 
+console.log(h);
+console.log(boo);
 h[4].outerHTML =  "<h2>            <a href='https://github.com/azat-io/you-dont-know-js-ru/blob/master/this%20%26%20object%20prototypes/README.md#you-dont-know-js-this--object-prototypes'target='_blank'>Книга 3. this и Прототипы Объектов</a></h2>";
 
 
-dav[6].remove('adv');
+adver.remove('adv');
 newElem.textContent = 'Глава 8: За пределами ES6” и поставить её в правильное место';
 elem[2].appendChild(newElem);
+
+ 
+col[0].insertBefore(boo[2], null);
+col[0].insertBefore(boo[9],null);
+
