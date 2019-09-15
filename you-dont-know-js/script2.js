04aa5e64962521d2e80fca72cd4c61623db7e17f
Nikitanv <54415506+Nikitanv@users.noreply.github.com>
2019-09-15 17:11:07 +0300

    Add files via upload

--- you-dont-know-js/script2.js
+++ you-dont-know-js/script2.js
@@ -1,38 +1,45 @@
 'use strict';
 
 
-let elem = document.querySelectorAll('.book'),
-col = document.querySelectorAll('.books'),
-adver = document.querySelector('.adv'),
-pick = document.querySelector('body'),
-h = document.querySelectorAll('h2'),
-newElem = document.createElement('li'),
-boo = elem[0].getElementsByTagName('li');
+ let books = document.querySelectorAll('.book'),
+ booksParent = document.querySelector('.books');
+ document.querySelector('.adv').remove();
+// pick = document.querySelector('body'),
+// h = document.querySelectorAll('h2'),
+// newElem = document.createElement('li'),
+ let title = books[0].getElementsByTagName('li');
+ let title1 = books[5].getElementsByTagName('li');
 
+booksParent.insertBefore(books[1],books[0]);
+booksParent.append(books[2]);
+booksParent.insertBefore(books[4],books[3]);
+// console.log(adver);
+// console.log(pick);
 
-console.log(adver);
-console.log(pick);
-col[0].insertBefore(elem[1],elem[0]);
-col[0].insertBefore(elem[2],null);
-col[0].insertBefore(elem[4],elem[3]);
+// console.log(document.children);
+console.log(books);
+document.body.style.backgroundImage = 'url(./image/you-dont-know-js.jpg)';
+books[4].getElementsByTagName('a')[0].textContent = 'Книга 3. this и Прототипы Объектов';
 
-console.log(document.children);
+// console.log(elem);
 
-pick.setAttribute('style', 'background-image:url(./image/you-dont-know-js.jpg)');
+// console.log(h);
+// console.log(book);
 
+// newElem.textContent = 'Глава 8: За пределами ES6” и поставить её в правильное место';
+// elem[2].appendChild(newElem);
+console.log(title);
+//books[0].insertBefore(book[2], book[9]); 
+//books[0].insertBefore(book[9],null);
+books[0].getElementsByTagName('ul')[0].insertBefore(title[2], title[10]);
+ books[0].getElementsByTagName('ul')[0].insertBefore(title[5], title[7]);
+ books[0].getElementsByTagName('ul')[0].insertBefore(title[7], title[3]);
+ books[0].getElementsByTagName('ul')[0].insertBefore(title[2], title[10]);
+ books[0].getElementsByTagName('ul')[0].insertBefore(title[9], title[6]);
+ books[0].getElementsByTagName('ul')[0].insertBefore(title[7], title[2]);
+ books[0].getElementsByTagName('ul')[0].insertBefore(title[7], title[2]);
+ console.log(title1);
 
-console.log(elem);
-
-console.log(h);
-console.log(boo);
-h[4].outerHTML =  "<h2>            <a href='https://github.com/azat-io/you-dont-know-js-ru/blob/master/this%20%26%20object%20prototypes/README.md#you-dont-know-js-this--object-prototypes'target='_blank'>Книга 3. this и Прототипы Объектов</a></h2>";
-
-
-adver.remove('adv');
-newElem.textContent = 'Глава 8: За пределами ES6” и поставить её в правильное место';
-elem[2].appendChild(newElem);
-
- 
-col[0].insertBefore(boo[2], boo[9]);
-col[0].insertBefore(boo[9],null);
-
+ books[5].getElementsByTagName('ul')[0].insertBefore(title1[9], title1[3]);
+ books[5].getElementsByTagName('ul')[0].insertBefore(title1[2], title1[7]);
+ books[5].getElementsByTagName('ul')[0].insertBefore(title1[5], title1[9]);
