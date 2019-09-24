966f920a867d0ab88563f17934e0381e92c0d5d4
Nikitanv <54415506+Nikitanv@users.noreply.github.com>
2019-09-24 20:14:26 +0300

    Add files via upload

--- you-dont-know-js/script2.js
+++ you-dont-know-js/script2.js
@@ -1,45 +1,32 @@
 'use strict';
 
-
- let books = document.querySelectorAll('.book'),
- booksParent = document.querySelector('.books');
- document.querySelector('.adv').remove();
-// pick = document.querySelector('body'),
-// h = document.querySelectorAll('h2'),
-// newElem = document.createElement('li'),
- let title = books[0].getElementsByTagName('li');
- let title1 = books[5].getElementsByTagName('li');
-
-booksParent.insertBefore(books[1],books[0]);
-booksParent.append(books[2]);
-booksParent.insertBefore(books[4],books[3]);
-// console.log(adver);
-// console.log(pick);
-
-// console.log(document.children);
-console.log(books);
-document.body.style.backgroundImage = 'url(./image/you-dont-know-js.jpg)';
-books[4].getElementsByTagName('a')[0].textContent = 'Книга 3. this и Прототипы Объектов';
-
-// console.log(elem);
-
-// console.log(h);
-// console.log(book);
-
-// newElem.textContent = 'Глава 8: За пределами ES6” и поставить её в правильное место';
-// elem[2].appendChild(newElem);
-console.log(title);
-//books[0].insertBefore(book[2], book[9]); 
-//books[0].insertBefore(book[9],null);
-books[0].getElementsByTagName('ul')[0].insertBefore(title[2], title[10]);
- books[0].getElementsByTagName('ul')[0].insertBefore(title[5], title[7]);
- books[0].getElementsByTagName('ul')[0].insertBefore(title[7], title[3]);
- books[0].getElementsByTagName('ul')[0].insertBefore(title[2], title[10]);
- books[0].getElementsByTagName('ul')[0].insertBefore(title[9], title[6]);
- books[0].getElementsByTagName('ul')[0].insertBefore(title[7], title[2]);
- books[0].getElementsByTagName('ul')[0].insertBefore(title[7], title[2]);
- console.log(title1);
-
- books[5].getElementsByTagName('ul')[0].insertBefore(title1[9], title1[3]);
- books[5].getElementsByTagName('ul')[0].insertBefore(title1[2], title1[7]);
- books[5].getElementsByTagName('ul')[0].insertBefore(title1[5], title1[9]);
+const DomElement = function(options, text){
+  this.selector = options.selector;
+  this.height = options.height ;
+  this.width = options.width ;
+  this.bg = options.bg ;
+  this.fontSize = options.fontSize ;
+  this.text = text;
+  };
+  
+  DomElement.prototype.createElement = function(){
+  console.log(this.selector[0]);
+  if(this.selector[0] ==='.'){
+  const div = document.createElement('div');
+  div.style.cssText = 'height:' + this.height + ';width:' + this.width + ';background:' +
+  this.bg + ';font-size:' + this.fontSize;
+  div.textContent = this.text;
+  document.body.appendChild(div);
+  }
+  };
+  const optionS = {
+  selector: '.square-blue',
+  height:'500px',
+  width:'500px',
+  bg:'blue',
+  fontSize:'26px'
+  };
+  
+  const round = new DomElement(optionS, 'синий как иней');
+  
+  round.createElement();
