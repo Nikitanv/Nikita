e95b04b3b967b62d9d0ff0cedba5c647a7615781
Nikitanv <54415506+Nikitanv@users.noreply.github.com>
2019-10-02 21:57:30 +0300

    Add files via upload

---
+++ lesson12/3dGlo/script.js
@@ -0,0 +1,89 @@
+window.addEventListener('DOMContentLoaded', function () {
+  'use strict';
+
+  //timer
+  function countTimer(deadline) {
+
+    let timerHours = document.querySelector('#timer-hours'),
+      timerMinutes = document.querySelector('#timer-minutes'),
+      timerSeconds = document.querySelector('#timer-seconds');
+
+    let dateStop = new Date(deadline).getTime(),
+      dateNow = new Date().getTime(),
+      timeRemaining = (dateStop - dateNow) / 1000,
+      seconds = Math.floor(timeRemaining % 60),
+      minutes = Math.floor((timeRemaining / 60) % 60),
+      hours = Math.floor(timeRemaining / 60 / 60);
+    if (hours < 10) {
+      hours = '0' + hours;
+    }
+    if (minutes < 10) {
+      minutes = '0' + minutes;
+    }
+    if (seconds < 10) {
+      seconds = '0' + seconds;
+    }
+
+
+
+    timerHours.textContent = hours;
+    timerMinutes.textContent = minutes;
+    timerSeconds.textContent = seconds;
+
+    if (timeRemaining < 0) {
+      clearInterval(idTimer);
+
+      timerHours.textContent = '00';
+      timerMinutes.textContent = '00';
+      timerSeconds.textContent = '00';
+    }
+
+  }
+  countTimer();
+
+  let idTimer = setInterval(countTimer, 1000, '1 octoder 2019');
+
+  //menu
+  const toggleMenu = () => {
+    const btnMenu = document.querySelector('.menu'),
+      menu = document.querySelector('menu'),
+      closeBtn = document.querySelector('.close-btn'),
+      menuItem = menu.querySelectorAll('ul>li');
+
+    const handlerMenu = () => {
+     menu.classList.toggle('active-menu');
+    };
+     closeBtn.addEventListener('click',handlerMenu);
+   
+       btnMenu.addEventListener('click',handlerMenu);
+    menuItem.forEach((elem)=> elem.addEventListener('click', handlerMenu));
+  };
+
+  toggleMenu();
+
+//popup
+const togglePopUp = () => {
+  const popup = document.querySelector('.popup'),
+  popupBtn = document.querySelectorAll('.popup-btn'),
+popUpClose = document.querySelector('.popup-close');
+
+  popupBtn.forEach((elem) =>{
+    elem.addEventListener('click', () =>{
+      popup.style.display = 'block';
+    });
+  });
+  popUpClose.addEventListener('click', () =>{
+    popup.style.display = 'none';
+  });
+};
+togglePopUp();
+
+
+
+
+
+
+
+
+
+});
